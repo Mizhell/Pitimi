d691ed5136949a59ca64703456a449a9b636431f
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-04-07 21:45:10 -0400

    Basic integration grunt and artisan.

---
+++ Gruntfile.js
@@ -0,0 +1,108 @@
+module.exports = function(grunt) {
+
+    'use strict';
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        watch: {
+            serve: {
+                files: [
+                    'Gruntfile.js',
+                    'public/assets/scss/**/*',
+                    'public/assets/js/**/*',
+                    'public/assets/img/**/*'
+                ],
+                tasks: ['jshint', 'sass:dev'],
+                options: {
+                    livereload: 8009
+                }
+            }
+        },
+
+        shell: {
+            artisanServe: {
+                options: {
+                    stdout: true
+                },
+                command: 'php artisan serve'
+            }
+        },
+
+        concurrent: {
+            watchers: {
+                tasks: ['watch', 'shell'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+
+        jshint: {
+            files: ['Gruntfile.js', 'public/assets/js/**/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: './public/assets/js/',
+                    name: '../public/bower_components/almond/almond',
+                    include: ['main'],
+                    insertRequire: ['main'],
+                    mainConfigFile: './public/assets/js/config.js',
+                    out: './public/assets/dist/main.js',
+                    findNestedDependencies: true,
+                    optimize: 'uglify2',
+                    preserveLicenseComments: false,
+                    wrap: {
+                        start: '(function () {',
+                        end: '}).call(this);'
+                    }
+                }
+            }
+        },
+
+        sass: {
+            serve: {
+                files: {
+                    'public/assets/css/main.css': 'public/assets/scss/main.scss'
+                },
+                options: {
+                    outputStyle: 'nested'
+                }
+            },
+            build: {
+                files: {
+                    'public/assets/dist/main.css': 'public/assets/scss/main.scss'
+                },
+                options: {
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+
+        imagemin: {
+            all: {
+                options: {
+                    optimizationLevel: 3,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: ['public/assets/img/**/**/*.png', 'public/assets/img/**/**/*.jpg'],
+                    dest: './',
+                }]
+            }
+        }
+
+    });
+
+    grunt.registerTask('serve', ['jshint', 'sass:serve', 'concurrent']);
+    grunt.registerTask('build', ['jshint', 'sass:build']);
+    grunt.registerTask('default', ['serve']);
+};
